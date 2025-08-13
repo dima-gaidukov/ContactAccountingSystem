27ff49a7b9c2ea0295377b19b613367c9c4267a5
dmitrijgajdukov <dima-gaidukov@mail.ru>
2025-08-14 00:58:37 +0300

    commit

--- src/main/java/Dima/d/ContactAccountingSystem.java
+++ src/main/java/Dima/d/ContactAccountingSystem.java
@@ -2,7 +2,7 @@ package Dima.d;
 
 import java.util.Scanner;
 
-public class Main {
+public class ContactAccountingSystem {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
